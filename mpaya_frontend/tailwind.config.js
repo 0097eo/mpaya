301ee8b446f4a8654bb2d9fd79c8d46a5eee0921
0097eo <emmanuelokello294@gmail.com>
2026-03-16 21:40:39 +0300

    chore: set up global styles and tailwind

---
+++ mpaya_frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'],
+        mono: ['DM Mono', 'monospace'],
+      },
+      colors: {
+        surface: '#FAFAF8',
+        border:  '#EAEAE4',
+        ink:     '#1A1A18',
+        muted:   '#A8A89C',
+        subtle:  '#52524A',
+        orange:  '#F97316',
+      },
+    },
+  },
+  plugins: [],
+}
