a15af5c25b8ad5ee6e673a254db942bead9bc987
0097eo <emmanuelokello294@gmail.com>
2026-03-17 13:57:59 +0300

    feat(auth): add IsSupport and IsAdminOrSupport permission classes

--- mpaya_api/apps/authentication/permissions.py
+++ mpaya_api/apps/authentication/permissions.py
@@ -10,3 +10,11 @@ class IsTechnician(BasePermission):
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == User.ADMIN
+    
+class IsSupport(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == User.SUPPORT
+
+class IsAdminOrSupport(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role in (User.ADMIN, User.SUPPORT)
