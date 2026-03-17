05d2b70d8871e70c75c3f049a534dee673bcd6d9
0097eo <emmanuelokello294@gmail.com>
2026-03-17 10:47:58 +0300

    fix: disable keep-alive

--- mpaya_frontend/vite.config.js
+++ mpaya_frontend/vite.config.js
@@ -9,6 +9,11 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
+        configure: (proxy) => {
+          proxy.on('proxyReq', (proxyReq) => {
+            proxyReq.setHeader('Connection', 'close')
+          })
+        }
       }
     }
   }
